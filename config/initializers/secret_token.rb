4cf96b9803628f04a5e8e943f538b8a4f3787ad3
fsainz <fsainz@gmail.com>
2011-05-19 12:50:40 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Walle::Application.config.secret_token = 'fd8b2861249f592e9c1dcc3f9c35d5d15412d9296ae9db05826afcae224a48cc01493dc7d5c1fbaaab5fbf3835438112f5180fc9bed4d97122b6907bc75501a0'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+Walle::Application.routes.draw do
+  root :to => 'home#index'
+end
